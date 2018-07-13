228370d04c1f8aca8bce89f4fd8cde9eb0e575ea
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-13 21:41:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,6 +12,4 @@ dog_action_one = Dog.new("Woof!")
 dog_action_two = Dog.new("The Dog is sitting")
 
 puts dog_action_one.bark
-puts dog_action_one.sit
 puts dog_action_two.sit 
-puts dog_action_two.bark 
