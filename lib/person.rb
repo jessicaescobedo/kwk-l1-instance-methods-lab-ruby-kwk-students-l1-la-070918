ad2c27c2e8ebf09a6199374e8578bcaddd64056c
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-13 21:50:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # person.rb
+class Person 
+  attr_accessor :talk, :walk
+  
+def initialize(talk,walk)
+  @talk = talk
+  @walk = walk
+  end 
+end 
+
+person_action = Person.new("Hello World!","The Person is walking.")
+
+puts person_action.bark
+puts person_action.sit 
